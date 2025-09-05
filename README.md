34f71e78a2fd1185504deaea8d2801bbb55c88b1
daria.daria <33233023+daria-z@users.noreply.github.com>
2025-09-05 16:00:22 +0500

    chore: configure local server and proxy for Vite

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/daria-z/frontend-project-12/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/daria-z/frontend-project-12/actions)
+
+/frontend
+vite `npm dev` `http://localhost:5173`
+
+/server
+@hexlet/chat-server `npm start-server` `http://localhost:5001`
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,37 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5001",
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, options) => {
+          proxy.on("error", (err, req, res) => {
+            console.error(`[Proxy] Error: ${err.message}`);
+            if (err.code === "ECONNREFUSED") {
+              res.writeHead(503); // Service Unavailable
+              res.end("Server is down");
+            } else {
+              res.writeHead(500);
+              res.end("Proxy error");
+            }
+          });
+        },
+      },
+      "/socket.io": {
+        target: "http://localhost:5001",
+        ws: true,
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, options) => {
+          proxy.on("error", (err, req, res) => {
+            console.error(`[Proxy] WebSocket Error: ${err.message}`);
+          });
+        },
+      },
+    },
+  },
+});
+
