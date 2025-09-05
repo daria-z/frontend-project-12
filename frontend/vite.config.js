7eacc55491082da1d2cd9afcf7a626474bd008bf
daria.daria <33233023+daria-z@users.noreply.github.com>
2025-09-05 17:53:04 +0500

    chore: set up build configuration with Makefile and concurrently

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: "5002",
     proxy: {
       "/api": {
-        target: "http://localhost:5001",
         changeOrigin: true,
         secure: false,
         configure: (proxy, options) => {
@@ -24,7 +24,6 @@ export default defineConfig({
         },
       },
       "/socket.io": {
-        target: "http://localhost:5001",
         ws: true,
         changeOrigin: true,
         secure: false,
