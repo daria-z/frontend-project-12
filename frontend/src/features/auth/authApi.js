56617511bbbf55369599174abbde54208d3ac52c
daria.daria <33233023+daria-z@users.noreply.github.com>
2025-09-08 19:35:59 +0500

    feat(auth): add prepareHeaders to include auth token in API requests

--- frontend/src/features/auth/authApi.js
+++ frontend/src/features/auth/authApi.js
@@ -3,6 +3,16 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5002" }),
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token;
+
+    // If we have a token set in state, let's assume that we should be passing it.
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+
+    return headers;
+  },
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (value) => ({
@@ -26,3 +36,4 @@ export const { useLoginMutation, useRegisterMutation } = authApi;
 // const [login, { isLoading, error }] = useLoginMutation();
 // await login({ username: 'admin', password: 'admin' }).unwrap(); // Отправляет POST на /api/v1/login
 // .unwrap() извлекает данные из ответа или выбрасывает ошибку, если запрос не удался.
+
